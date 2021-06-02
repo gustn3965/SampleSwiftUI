8502ef9f003fd5101ec09b7cceba101d05430f7d
gustn3965 <gustn3965@gmail.com>
2021-06-02 21:25:05 +0900

    Initial Commit

---
+++ Shared/SampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwiftUIApp.swift
+//  Shared
+//
+//  Created by hyunsu on 2021/06/02.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
